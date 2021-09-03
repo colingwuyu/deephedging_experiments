a9ba04f3df5bd332b1a2e49b3f6d2fb0e188e2ec
Zeyu <colingwuyu@gmail.com>
2021-09-03 03:36:55 +0000

    update workspace

--- ln-ext.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ -d "/root/.vscode-server/extensions" ]; then rm -Rf /root/.vscode-server/extensions; fi
-ln -s /content/gdrive/MyDrive/vscode/extensions /root/.vscode-server/
-
-git config --global user.name "Zeyu Wang"
-git config --global user.email "colingwuyu@gmail.com"
